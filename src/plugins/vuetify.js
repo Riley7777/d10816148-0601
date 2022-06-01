682af4c5e026617a4e40034742f04d22ab55bbe1
Riley <confusedly0814@gmail.com>
2022-06-01 16:46:49 +0800

    frist

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,15 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme:{
+        themes:{
+            light:{
+                primary:"#000000",
+                se:"#484848",
+                gray:"#8d8d8d8d",
+                white:"#fffff",
+                bg:"#EFEFEF"
+            }
+        }
+    }
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,23 @@
 <template>
-  <hello-world />
+  <div  class="se">
+  <h1 >H1</h1>
+  <h2>H2</h2>
+  <h3>H3</h3>
+  <h4>H4</h4>
+  <h5>H5</h5>
+  <h6>H6</h6>
+  <div class="white--text">
+      <h3>BBBB</h3>
+  </div>
+  </div>
 </template>
-
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
-  export default {
-    name: 'Home',
+export default {
+  data(){
+    return{
 
-    components: {
-      HelloWorld,
-    },
+    }
   }
+}
 </script>
